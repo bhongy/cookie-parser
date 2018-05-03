f02227f4c048f469164dc67acfb7c4ad88118df3
Thanik Bhongbhibhat <thanikb@pinterest.com>
2018-05-03 09:14:55 -0700

    Implement basic cookie parsing (without JSONCookie)

--- src/cookie-parser.js
+++ src/cookie-parser.js
@@ -1,10 +1,14 @@
 'use strict';
 
+const cookie = require('cookie');
+
 module.exports = cookieParser;
 
 function cookieParser () {
   return function cookieParserMiddleware (req, res, next) {
-    req.cookies = Object.create(null);
+    req.cookies = req.headers.cookie
+      ? cookie.parse(req.headers.cookie)
+      : Object.create(null);
     req.signedCookies = Object.create(null);
     next();
   };
--- src/cookie-parser.test.js
+++ src/cookie-parser.test.js
@@ -2,6 +2,12 @@ const express = require('express');
 const supertest = require('supertest');
 const cookieParser = require('./cookie-parser');
 
+/**
+ * notes:
+ * - don't test status code - cookieParser is agnostic of status code
+ *   (it is not responsible to set status code)
+ */
+
 describe('cookieParser()', () => {
   let app;
 
@@ -57,4 +63,38 @@ describe('cookieParser()', () => {
         });
     });
   });
+
+  describe('when cookies are sent', () => {
+    it('should populate req.cookies', () => {
+      expect.assertions(1);
+      return supertest(app)
+        .get('/')
+        .set('Cookie', 'foo=bar; bar=baz')
+        .then(res => {
+          expect(res.body.cookies).toEqual({ foo: 'bar', bar: 'baz' });
+        });
+    });
+
+    // it('should inflate JSON cookies', () => {
+    //   expect.assertions(1);
+    //   return supertest(app)
+    //     .get('/')
+    //     .set('Cookie', 'moo=j:{"foo":"bar"}')
+    //     .then(res => {
+    //       expect(res.body.cookies).toEqual({ moo: { foo: 'bar' } });
+    //     });
+    // });
+
+    it('should not inflate invalid JSON cookies', () => {
+      expect.assertions(1);
+      return supertest(app)
+        .get('/')
+        .set('Cookie', 'moo=j:{"foo":')
+        .then(res => {
+          expect(res.body.cookies).toEqual({ moo: 'j:{"foo":' });
+        });
+    });
+  });
+});
+
 });
